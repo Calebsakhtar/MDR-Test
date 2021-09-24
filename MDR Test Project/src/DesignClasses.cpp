be37d8bfee218c60b10eb0b34ad820f62d225f01
Calebsakhtar <calebsakhtar@gmail.com>
2021-09-24 17:40:34 +0200

    Finish the basic design classes

--- MDR Test Project/src/DesignClasses.cpp
+++ MDR Test Project/src/DesignClasses.cpp
@@ -11,62 +11,110 @@ namespace MDR {
 		size_t m_num = 0;
 
 	public:
-		// Default constructor
+		// Default constructor (constructs an empty object)
 		MetricID() {};
 
-		MetricID(const std::string name, const size_t num) {
+		// Intended constructor
+		MetricID(const std::string& name, const size_t& num) {
 			m_name = name;
 			size_t m_num = num;
 		}
 
-		void set(const std::string name, const size_t num) {
+		void set(const std::string& name, const size_t& num) {
 			m_name = name;
 			size_t m_num = num;
 		}
 
-		std::string get_name() {
+		std::string get_name() const {
 			return m_name;
 		}
 
-		size_t get_num() {
+		size_t get_num() const {
 			return m_num;
 		}
 	};
 
 	class PerfMetric {
-		MetricID m_id;
+		MetricID m_metric_id;
 		double m_val = 0;
+		bool m_minimize = true;
 
 	public:
-		// Default constructor
+		// Default constructor (constructs an empty object)
 		PerfMetric() {}
 
-		PerfMetric(MetricID id, const double val) {
-			m_id.set(id.get_name(), id.get_num());
+		// Intended constructor
+		PerfMetric(const MetricID& id, const double& val, const bool& minimize) {
+			m_metric_id.set(id.get_name(), id.get_num());
+			m_val = val;
+			m_minimize = minimize;
+		}
+
+		void set(const MetricID& id, const double& val, const bool& minimize) {
+			m_metric_id.set(id.get_name(), id.get_num());
+			m_val = val;
+			m_minimize = minimize;
+		}
+
+		void set_id(const MetricID& id) {
+			m_metric_id.set(id.get_name(), id.get_num());
+		}
+
+		void set_val(const double& val) {
 			m_val = val;
 		}
 
-		void set(MetricID id, const double val) {
-			m_id.set(id.get_name(), id.get_num());
-			m_val = val;
+		void set_minimize(const double& minimize) {
+			m_minimize = minimize;
 		}
 
-		void set_id(MetricID id) {
-			m_id.set(id.get_name(), id.get_num());
+		std::string get_metric_name() const {
+			return m_metric_id.get_name();
 		}
 
-		void set_val(const double val) {
-			m_val = val;
+		size_t get_metric_id() const {
+			return m_metric_id.get_num();
 		}
 
-		std::string get_metric_name() {
-			return m_id.get_name();
+		bool get_metric_minimize() const {
+			return m_minimize;
+		}
+	};
+
+	class Design {
+		std::vector<PerfMetric> m_perf_vector;
+		size_t m_design_id = 0;
+
+	public:
+		// Default constructor (constructs an empty object)
+		Design() {}
+
+		// Constructor to be used for copying
+		Design(const std::vector<PerfMetric>& perf_vect, const size_t& design_id) {
+			m_perf_vector = perf_vect;
+			m_design_id = design_id;
 		}
 
-		size_t get_metric_id_num() {
-			return m_id.get_num();
+		// Constructor for normal use
+		Design(const size_t& design_id) {
+			m_design_id = design_id;
 		}
 
+		void add_perf_metric(const PerfMetric& perf_metric) {
+			m_perf_vector.push_back(perf_metric);
+		}
+
+		void set_perf_vector(const std::vector<PerfMetric>& perf_vector) {
+			m_perf_vector = perf_vector;
+		}
+
+		size_t get_design_id() const {
+			return m_design_id;
+		}
+
+		std::vector<PerfMetric> get_perf_vector() const {
+			return m_perf_vector;
+		}
 
 	};
 }
