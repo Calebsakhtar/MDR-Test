abf574a536f62a58051bd732d5ec5808525ea1a4
Calebsakhtar <calebsakhtar@gmail.com>
2021-11-03 17:59:42 +0000

    Add a python script to finish translating data

--- MDR Test Project/CSV_op.m
+++ MDR Test Project/CSV_op.m
@@ -4,6 +4,6 @@ close all;
 load([pwd,'\Memory.mat']);
 temp = [LTM.performance];
 write = [[temp.L_D]' [temp.M_empty]' [temp.MTOW]' [temp.Stall]'];
-write(isnan(write))=10000000000000000000;
+write(isnan(write))=100000000;
 csvwrite('MATLAB_OP.csv', write);
 
