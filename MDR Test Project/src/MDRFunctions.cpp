d2154a56099cacfa3d9903d94e3e90f854818c2e
Calebsakhtar <46966291+Calebsakhtar@users.noreply.github.com>
2022-03-12 11:47:52 +0000

    Revert "Ca525 improve functionality (#8)"
    This reverts commit 1f060af87c13eedf013387d0d55f3f3c7887c16d.

--- MDR Test Project/src/MDRFunctions.cpp
+++ MDR Test Project/src/MDRFunctions.cpp
@@ -60,24 +60,23 @@ namespace MDR {
 		assert(second_minimize_A == second_minimize_B); // Check for errors in minimize
 
 		// Evaluate the dominance relation
-		const double tol = 1e6;
 
 		bool first_dominance = false;
 		if (first_minimize_A) {
 			// If the 1st value is to be minimized, check whether A's 1st value is the smaller one.
-			first_dominance = tol < (first_perf_val_B - first_perf_val_A);
+			first_dominance = first_perf_val_A < first_perf_val_B;
 		}
 		else {
-			first_dominance = (first_perf_val_A - first_perf_val_B) > tol;
+			first_dominance = first_perf_val_A > first_perf_val_B;
 		}
 
 		bool second_dominance = false;
 		if (second_minimize_A) {
 			// If the 2nd value is to be minimized, check whether A's 2nd value is the smaller one.
-			second_dominance = tol < (second_perf_val_B - second_perf_val_A);
+			second_dominance = second_perf_val_A < second_perf_val_B;
 		}
 		else {
-			second_dominance = (second_perf_val_A - second_perf_val_B) > tol;
+			second_dominance = second_perf_val_A > second_perf_val_B;
 		}
 
 		// MDR considers relations in pairs (this is the smart bit)
@@ -132,24 +131,23 @@ namespace MDR {
 			assert(second_minimize_A == second_minimize_B); // Check for errors in minimize
 
 			// Evaluate the dominance relation
-			const double tol = 1e6;
 
 			bool first_dominance = false;
 			if (first_minimize_A) {
 				// If the 1st value is to be minimized, check whether A's 1st value is the smaller one.
-				first_dominance = tol < (first_perf_val_B - first_perf_val_A);
+				first_dominance = first_perf_val_A < first_perf_val_B;
 			}
 			else {
-				first_dominance = (first_perf_val_A - first_perf_val_B) > tol;
+				first_dominance = first_perf_val_A > first_perf_val_B;
 			}
 
 			bool second_dominance = false;
 			if (second_minimize_A) {
 				// If the 2nd value is to be minimized, check whether A's 2nd value is the smaller one.
-				second_dominance = tol < (second_perf_val_B - second_perf_val_A);
+				second_dominance = second_perf_val_A < second_perf_val_B;
 			}
 			else {
-				second_dominance = (second_perf_val_A - second_perf_val_B) > tol;
+				second_dominance = second_perf_val_A > second_perf_val_B;
 			}
 
 			// MDR considers relations in pairs (this is the smart bit)
