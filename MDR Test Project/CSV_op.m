fe7e29d6c2a3688552e195f07431389ca8713dd6
Caleb AM <calebsakhtar@gmail.com>
2022-05-31 21:53:22 +0100

    Delete unnecessary files

--- MDR Test Project/CSV_op.m
+++
@@ -1,9 +0,0 @@
-clear all;
-close all;
-
-load([pwd,'\Memory.mat']);
-temp = [LTM.performance];
-write = [[temp.L_D]' [temp.M_empty]' [temp.MTOW]' [temp.Stall]'];
-write(isnan(write))=100000000;
-csvwrite('MATLAB_OP.csv', write);
-
